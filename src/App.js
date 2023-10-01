93a4c25181ddb23d412ea96d70efe06a23157d74
Gianna <gvc222@nyu.edu>
2023-09-30 22:04:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Navbar } from './components/navbar';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Shop } from "./pages/shop/shop"
+import { Cart } from "./pages/cart/cart"
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/products.js
@@ -0,0 +1,67 @@
+import elixir from "./assets/elixir.png"
+import fire from "./assets/fire.png"
+import greaterHealing from "./assets/greater-healing.png"
+import hp from "./assets/hp.png"
+import lesserHealing from "./assets/lesser-healing.png"
+import magic from "./assets/magic.png"
+import mana from "./assets/mana.png"
+import poison from "./assets/poison.png"
+import protection from "./assets/protection.png"
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Elixir",
+        price: 100,
+        productImage: elixir
+    },
+    {
+        id: 2,
+        productName: "Fire Potion",
+        price: 50,
+        productImage: fire
+    },
+    {
+        id: 3,
+        productName: "Potion of Greater Healing",
+        price: 75,
+        productImage: greaterHealing
+    },
+    {
+        id: 4,
+        productName: "HP Potion",
+        price: 50,
+        productImage: hp
+    },
+    {
+        id: 5,
+        productName: "Potion of Lesser Healing",
+        price: 25,
+        productImage: lesserHealing
+    },
+    {
+        id: 6,
+        productName: "Potion of Magic",
+        price: 50,
+        productImage: magic
+    },
+    {
+        id: 7,
+        productName: "Potion of Mana",
+        price: 50,
+        productImage: mana
+    },
+    {
+        id: 8,
+        productName: "Poison",
+        price: 25,
+        productImage: poison
+    },
+    {
+        id: 9,
+        productName: "Potion of Protection",
+        price: 100,
+        productImage: protection
+    }
+
+]
